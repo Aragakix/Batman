971869111ecbc863a0f1a96d697a5d1c0df98924
Felipe Aragaki <Felipearagaki1@gmail.com>
2024-02-01 12:06:17 -0300

    Novo teste merge entre branchs

--- teste.py
+++ teste.py
@@ -3,3 +3,10 @@
 print("Olá Python no git")
 
 print("Estou aprendendo GIT2")
+
+
+
+print("Testando marge entre branchs")
+
+
+print("Depois sincronizar o teste com a branch principal e subir para o git com push")
